c3f2a0ae471ee66a1f086c26dc7d5a1d02dabc80
Jiamin-Zou <jiaminz622@gmail.com>
2023-04-14 16:44:43 -0400

    Completed Cats model and validations

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -1,13 +1,22 @@
+require 'action_view'
+require 'action_view/helpers'
+
 class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
 
     CAT_COLORS = ["Orange", "White", "Brouwn", "Black", "Black and White"]
     validates :birth_date, :color, :name, :sex, presence: true
     validates :color, inclusion: { in: CAT_COLORS }
     validates :sex, inclusion: { in: %w(M F) }
+    validate :birth_date_cannot_be_future
 
     def birth_date_cannot_be_future
-        if birth_date.present? && birth_date > Date.today
-          errors.add(:birth_date, "can't be in the past")
-        end
+      if birth_date.present? && birth_date > Date.today
+        errors.add(:birth_date, "can't be in the future")
       end
+    end
+
+    def age
+      time_ago_in_words(birth_date)
+    end
 end
