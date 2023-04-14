21e65778ce6104e8df9709f712fcc9685f809c67
Naoko Sugawara <infoseek122@gmail.com>
2023-04-13 21:51:04 -0400

    added cats_controller

---
+++ ninety-nine-cats/app/controllers/cats_controller.rb
@@ -0,0 +1,38 @@
+class CatsController < ApplicationController
+
+    def index
+        @cats = Cat.all
+        render :index
+    end
+
+    def show
+        @cat = Cat.find(params[:id])
+        render :show
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages, status: 422
+        end
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+
+    def cat_params
+        params.require(:cat).permit(:name, :color, :birth_date, :sex, :description)
+    end
+
+end
